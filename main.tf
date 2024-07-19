72b048d1333e6b6484428e82f776d37b597204a4
yadavprakash <yadavprakash.it@gmail.com>
2024-07-19 11:24:41 +0530

    update module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 #Module      : LABEL
 
 module "labels" {
-  source      = "git::https://github.com/opsstation/terraform-digitalocean-labels.git?ref=v1.0.0"
+  source      = "git::https://github.com/yadavprakash/terraform-digitalocean-labels.git?ref=master"
   name        = var.name
   environment = var.environment
   label_order = var.label_order
